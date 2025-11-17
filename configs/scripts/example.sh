5a933b5cbc6e8c804da0b651e52518b5f8984a6d
HC <s224174@dtu.dk>
2025-11-17 14:17:10 +0100

    batch scripts

---
+++ configs/scripts/example.sh
@@ -0,0 +1,27 @@
+### Example of batch script
+
+#!/bin/sh
+### General options
+### -- specify queue --
+#BSUB -q hpc
+
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+### -- set walltime limit: hh:mm
+#BSUB -W 01:00
+### request 2GB of system-memory
+#BSUB -R "rusage[mem=2GB]"
+
+### -- set the job Name --
+#BSUB -J gnn_train
+### -- Specify the output and error file. %J is the job-id --
+#BSUB -o batch/logs/test%J.out
+#BSUB -e batch/logs/test%J.err
+
+# -- end of LSF options --
+
+### nvidia-smi
+
+source ~/miniconda3/bin/activate gnn-qm9
+python src/run.py
